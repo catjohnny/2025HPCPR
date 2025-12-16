6a9ba91058eca30aec9a4a48ad52a5deec48a396
catjohnny <cat110152@gmail.com>
2025-12-16 09:21:02 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 如果您的倉庫名稱是 my-repo，請將 '/' 改為 '/my-repo/'
-  // 為了通用性，我們可以使用相對路徑 './'
-  base: './',
+  base: '/2025HPCPR/',
   build: {
     outDir: 'dist',
+    emptyOutDir: true
   }
 });
